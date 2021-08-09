86135a6191a8f2f1317a7eb2805b6e1781eb837a
Mark-Kamitz <85449060+Mark-Kamitz@users.noreply.github.com>
2021-08-09 15:21:52 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -122,51 +122,51 @@
             </div><div class='mt-5'><table class='table table-dark table-striped'>
                 <thead>
                     <tr>
-                    <th scope='col'>#</th>
-                    <th scope='col'>Brand</th>
-                    <th scope='col'>Type</th>
-                    <th scope='col'>Build</th>
-                    <th scope='col'>Price</th>
-                    <th scope='col'>Available</th>
-                    <th scope='col'>Location</th>
+                        <th scope='col'>#</th>
+                        <th scope='col'>Brand</th>
+                        <th scope='col'>Type</th>
+                        <th scope='col'>Build</th>
+                        <th scope='col'>Price</th>
+                        <th scope='col'>Available</th>
+                        <th scope='col'>Location</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                    <th scope='row'>1</th>
-                    <td>".$carRentals['Audi']['name']."</td>
-                    <td>".$carRentals['Audi']['type']."</td>
-                    <td>".$carRentals['Audi']['date']."</td>
-                    <td>".$carRentals['Audi']['price']."</td>
-                    <td>".$carRentals['Audi']['available']."</td>
-                    <td>".$carRentals['Audi']['location']."</td>
+                        <th scope='row'>1</th>
+                        <td>".$carRentals['Audi']['name']."</td>
+                        <td>".$carRentals['Audi']['type']."</td>
+                        <td>".$carRentals['Audi']['date']."</td>
+                        <td>".$carRentals['Audi']['price']."</td>
+                        <td>".$carRentals['Audi']['available']."</td>
+                        <td>".$carRentals['Audi']['location']."</td>
                     </tr>
                     <tr>
-                    <th scope='row'>2</th>
-                    <td>".$carRentals['Lambo']['name']."</td>
-                    <td>".$carRentals['Lambo']['type']."</td>
-                    <td>".$carRentals['Lambo']['date']."</td>
-                    <td>".$carRentals['Lambo']['price']."</td>
-                    <td>".$carRentals['Lambo']['available']."</td>
-                    <td>".$carRentals['Lambo']['location']."</td>
+                        <th scope='row'>2</th>
+                        <td>".$carRentals['Lambo']['name']."</td>
+                        <td>".$carRentals['Lambo']['type']."</td>
+                        <td>".$carRentals['Lambo']['date']."</td>
+                        <td>".$carRentals['Lambo']['price']."</td>
+                        <td>".$carRentals['Lambo']['available']."</td>
+                        <td>".$carRentals['Lambo']['location']."</td>
                     </tr>
                     <tr>
-                    <th scope='row'>3</th>
-                    <td>".$carRentals['Porsche']['name']."</td>
-                    <td>".$carRentals['Porsche']['type']."</td>
-                    <td>".$carRentals['Porsche']['date']."</td>
-                    <td>".$carRentals['Porsche']['price']."</td>
-                    <td>".$carRentals['Porsche']['available']."</td>
-                    <td>".$carRentals['Porsche']['location']."</td>
+                        <th scope='row'>3</th>
+                        <td>".$carRentals['Porsche']['name']."</td>
+                        <td>".$carRentals['Porsche']['type']."</td>
+                        <td>".$carRentals['Porsche']['date']."</td>
+                        <td>".$carRentals['Porsche']['price']."</td>
+                        <td>".$carRentals['Porsche']['available']."</td>
+                        <td>".$carRentals['Porsche']['location']."</td>
                     </tr>
-                    <tr>
-                    <th scope='row'>4</th>
-                    <td>".$carRentals['BMW']['name']."</td>
-                    <td>".$carRentals['BMW']['type']."</td>
-                    <td>".$carRentals['BMW']['date']."</td>
-                    <td>".$carRentals['BMW']['price']."</td>>
-                    <td>".$carRentals['BMW']['available']."</td>
-                    <td>".$carRentals['BMW']['location']."</td>
+                        <tr>
+                        <th scope='row'>4</th>
+                        <td>".$carRentals['BMW']['name']."</td>
+                        <td>".$carRentals['BMW']['type']."</td>
+                        <td>".$carRentals['BMW']['date']."</td>
+                        <td>".$carRentals['BMW']['price']."</td>>
+                        <td>".$carRentals['BMW']['available']."</td>
+                        <td>".$carRentals['BMW']['location']."</td>
                     </tr>
                 </tbody>
                 </table>
